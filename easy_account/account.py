b31ce39b3cab4df43e4717d4bad721dec0f2ad2c
Nicolas VINCENT <nico.vince@gmail.com>
2026-03-22 05:18:05 +0100

    account: get month cell
    add method to get the cell of a month

--- easy_account/account.py
+++ easy_account/account.py
@@ -1,6 +1,7 @@
 """Manage Account Spreadsheet."""
 
 import openpyxl
+from openpyxl.cell.cell import Cell
 
 
 class AccountSpreadsheet:
@@ -10,8 +11,9 @@ class AccountSpreadsheet:
         self.path = spreadsheet_path
         self.wb = openpyxl.load_workbook(self.path)
         self.category_column = "A"
+        self.month_row = 1
 
-    def get_cell_category(self, category: str):
+    def get_cell_category(self, category: str) -> Cell:
         """Get the cell for a matching category."""
         ws = self.wb.active
         categories = ws[self.category_column]
@@ -19,3 +21,12 @@ class AccountSpreadsheet:
             if cell.value == category:
                 return cell
         assert False, f"{category} not found in {self.path}"
+
+    def get_cell_month(self, month: str) -> Cell:
+        """Return the cell for a matching month."""
+        ws = self.wb.active
+        months = ws[self.month_row]
+        for cell in months:
+            if cell.value == month:
+                return cell
+        assert False, f"{month} not found in {self.path}"
--- tests/test_account_spreadsheet.py
+++ tests/test_account_spreadsheet.py
@@ -7,7 +7,7 @@ def dummy_account():
     return AccountSpreadsheet("tests/dummy_account.xlsx")
 
 
-def test_constructor():
+def test_account_constructor():
     AccountSpreadsheet("tests/dummy_account.xlsx")
 
 
@@ -19,3 +19,13 @@ def test_account_get_row_from_category(dummy_account):
 def test_account_invalid_category(dummy_account):
     with pytest.raises(AssertionError):
         dummy_account.get_cell_category("dsmfkj")
+
+
+def test_account_get_cell_month(dummy_account):
+    cell = dummy_account.get_cell_month("janvier")
+    assert cell.column_letter == "B"
+
+
+def test_account_get_cell_month_invalid(dummy_account):
+    with pytest.raises(AssertionError):
+        dummy_account.get_cell_month("qmlfkj")
